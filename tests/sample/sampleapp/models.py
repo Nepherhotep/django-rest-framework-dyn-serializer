235e8945715f22e799e05998cc2a62dbe2069dd2
Alexey Zankevich <zancudero@gmail.com>
2016-06-20 17:58:18 +0300

    Added sample app for tests

---
+++ tests/sample/sampleapp/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=50)
+    birth_date = models.DateField(blank=True, null=True)
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=255)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    content = models.TextField()
+    author = models.ForeignKey(Author, related_name='articles')
+
+
+class Review(models.Model):
+    summary = models.CharField(max_length=255)
+    content = models.TextField()
+    stars = models.IntegerField()
