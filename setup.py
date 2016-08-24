12fa4b91b8a49a2f9e7386cbf9047cd15736fb43
Alexey Zankevich <zancudero@gmail.com>
2016-08-24 12:41:02 +0300

    Added setup file

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+__author__ = 'Alexey Zankevich'
+
+
+setup(
+    name="rest_framework_dyn_serializer",
+    version="1.0.0",
+    py_modules=['rest_framework_dyn_serializer'],
+    author="Alexey Zankevich",
+    author_email="alex.zankevich@gmail.com",
+    description="Dynamic serializer for Django REST framework",
+    keywords=['Django', 'REST', 'DRF'],
+    license="MIT",
+    platforms=['Platform Independent'],
+    url="https://github.com/Nepherhotep/django-rest-framework-dyn-serializer",
+    install_requires=['django', 'djangorestframework'],
+    classifiers=["Framework :: Django :: 1.8",
+                 "Framework :: Django :: 1.9",
+                 "Framework :: Django :: 1.10",
+                 "Development Status :: 5 - Production/Stable",
+                 "Programming Language :: Python :: 2.7",
+                 "Programming Language :: Python :: 3.5"]
+)
