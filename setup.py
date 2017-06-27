9d0a8ce9ef875570426f5298ecb947b09153cfbd
Alexey Zankevich <zancudero@gmail.com>
2017-06-27 15:35:04 +0300

    Updated dyn serializer version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ __author__ = 'Alexey Zankevich'
 
 setup(
     name="rest_framework_dyn_serializer",
-    version="1.0.2",
+    version="1.1.0",
     py_modules=['rest_framework_dyn_serializer'],
     author="Alexey Zankevich",
     author_email="alex.zankevich@gmail.com",
